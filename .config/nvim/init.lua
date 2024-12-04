5dc340c30b594aa231d57a1765561a1f5e811f2d
Robert Schulze <robert@clickhouse.com>
2024-12-04 14:47:48 +0000

    Migrate to mini.deps

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -67,199 +67,171 @@ vim.cmd([[au FileType sql lua vim.opt_local.commentstring = '-- %s']])
 -- ----------------------------------------------------------------------
 -- Plug-ins
 
--- Auto-install lazy.nvim plug-in manager, cf. https://github.com/folke/lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+local path_package = vim.fn.stdpath('data') .. '/site/'
+local mini_path = path_package .. 'pack/deps/start/mini.nvim'
+if not vim.loop.fs_stat(mini_path) then
+  vim.cmd('echo "Installing `mini.nvim`" | redraw')
+  local clone_cmd = {
+    'git', 'clone', '--filter=blob:none',
+    'https://github.com/echasnovski/mini.nvim', mini_path
+  }
+  vim.fn.system(clone_cmd)
+  vim.cmd('packadd mini.nvim | helptags ALL')
+  vim.cmd('echo "Installed `mini.nvim`" | redraw')
 end
-vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup({
-    {
-        'ellisonleao/gruvbox.nvim',
-        priority = 1000, -- ... as the first plug-in
-        config = function()
-            vim.cmd.colorscheme('gruvbox')
-        end
-    },
-    {
-        'echasnovski/mini.nvim',
-        config = function()
-            require('mini.bracketed').setup()
-            require('mini.jump2d').setup()
-            require('mini.misc').setup()
-            MiniMisc.setup_auto_root()
-            require('mini.move').setup()
-            require('mini.pairs').setup()
-            require('mini.surround').setup()
-            require('mini.indentscope').setup({
-                draw = { animation = function() return 0 end },
-                symbol ='│'
-            })
-            -- TODO try pick and extra
-        end
-    },
-    -- {
-    --     -- TODO: replace by Lua equivalent
-    --     -- See :Copilot setup / status
-    --     -- 'github/copilot.vim'
-    --     -- 'zbirenbaum/copilot.lua',
-    -- },
-    {
-        -- Remember last cursor position
-        -- https://github.com/neovim/neovim/issues/16339
-        'ethanholz/nvim-lastplace',
-        config = function()
-            require('nvim-lastplace').setup()
-        end
-    },
-    {
-        "FabijanZulj/blame.nvim",
-        config = function()
-            require("blame").setup()
-        end
-    };
-    {
-        'nvim-telescope/telescope.nvim',
-        dependencies = 'nvim-lua/plenary.nvim',
-        config = function()
-            local actions = require('telescope.actions')
-            require 'telescope'.setup({
-              defaults = {
-                preview = false,
-                mappings = {
-                  i = {
-                    ['<esc>'] = actions.close, -- close on single <esc> press (instead of two)
-                  },
-                },
-              },
-              pickers = {
-                git_files = {
-                  theme = 'ivy',
-                },
-                buffers = {
-                  theme = 'ivy',
-                  sort_mru = true,
-                },
-                live_grep = {
-                  theme = 'ivy',
-                  preview = true
-                }
-              },
-            })
-            vim.keymap.set('n', '<Leader>e', require('telescope.builtin').git_files)
-            vim.keymap.set('n', '<Leader>b', require('telescope.builtin').buffers)
-            vim.keymap.set('n', '<Leader>l', require('telescope.builtin').live_grep) -- requires ripgrep
-        end
-    },
-    {
-        'hiphish/rainbow-delimiters.nvim'
-    },
-    {
-        'nvim-treesitter/nvim-treesitter',
-        build = ':TSUpdate',
-        config = function()
-            require'nvim-treesitter.configs'.setup({
-                ensure_installed = 'all',
-                highlight = {enable = true}
-            })
-        end
-    },
-    {
-        -- TODO: migrate to LSP Zero one fine day
-        'neovim/nvim-lspconfig',
-        dependencies = {
-            -- TODO add a snippet engine
-            'hrsh7th/nvim-cmp',
-            'hrsh7th/cmp-buffer',
-            'hrsh7th/cmp-nvim-lsp',
-            'hrsh7th/cmp-nvim-lsp-signature-help',
-            'williamboman/mason-lspconfig.nvim',
-            'williamboman/mason.nvim'
-        },
-        config = function()
-            -- Install LSP servers from within nvim, check the status with :LspInfo and :LspInstallInfo
-            require('mason').setup()
-            require('mason-lspconfig').setup({
-                ensure_installed = { 'clangd' },
-                automatic_installation = true
-            })
+require('mini.deps').setup({ path = { package = path_package } })
 
-            -- Keymaps to expose some LSP features, many other functions are available ...
-            local function on_attach(_, bufnr)
-                local opts = { buffer = bufnr }
-                -- vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts) -- neovim 0.10 sets this by default
-                vim.keymap.set('n', 's', vim.lsp.buf.definition, opts)
-                vim.keymap.set('n', 't', vim.lsp.buf.type_definition, opts)
-                vim.keymap.set('n', 'S', ':ClangdSwitchSourceHeader<CR>', opts)
-                vim.keymap.set('n', 'R', vim.lsp.buf.references, opts)
-                vim.keymap.set('n', '<Leader>r', vim.lsp.buf.rename, opts)
-                vim.keymap.set('n', '<C-p>', vim.diagnostic.goto_prev, opts)
-                vim.keymap.set('n', '<C-n>', vim.diagnostic.goto_next, opts)
-            end
+--
 
-            -- Advertise capabilities to LSP server
-            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+MiniDeps.add('ellisonleao/gruvbox.nvim')
+vim.cmd('colorscheme gruvbox')
 
-            -- Disable messages in sign column by LSP plugin, virtual text does the job nicely
-            vim.diagnostic.config({signs = false})
+require('mini.bracketed').setup()
+require('mini.indentscope').setup({
+    draw = { animation = function() return 0 end },
+    symbol ='│'
+})
+require('mini.jump2d').setup()
+require('mini.misc').setup()
+MiniMisc.setup_auto_root()
+require('mini.move').setup()
+require('mini.pairs').setup()
+require('mini.surround').setup()
+    -- TODO pick, extra
 
-            require('lspconfig')['clangd'].setup({
-                on_attach = on_attach,
-                capabilities = capabilities
-            })
+-- Remember last cursor position
+-- https://github.com/neovim/neovim/issues/16339
+MiniDeps.add('ethanholz/nvim-lastplace')
+require('nvim-lastplace').setup()
 
-            -- Note from https://clangd.llvm.org/installation.html:
-            -- "clangd will look in the parent directories of the files you edit looking
-            -- for it, and also in subdirectories named build/. For example, if editing
-            -- $SRC/gui/window.cpp, we search in $SRC/gui/, $SRC/gui/build/, $SRC/,
-            -- $SRC/build/,"
-            -- If this becomes too annoying, we could pass --compile-commands-dir=<string>
-            -- to clangd above (--> "cmd")
+MiniDeps.add('FabijanZulj/blame.nvim')
+require('blame').setup()
 
-            -- local check_backspace = function()
-            --     local col = vim.fn.col '.' - 1
-            --     return col == 0 or vim.fn.getline('.'):sub(col, col):match '%s'
-            -- end
+-- MiniDeps.add('hiphish/rainbow-delimiters.nvim')
 
-            local cmp = require 'cmp'
-            cmp.setup({
-                -- The order controls the preference for specific sources
-                sources = {
-                    {name = 'nvim_lsp'},
-                    {name = 'nvim_lsp_signature_help'},
-                    {name = 'buffer'}
-                },
-                mapping = {
-                    -- Overload tab for a natural completion experience
-                    ['<Tab>'] = function(fallback)
-                      if cmp.visible() then
-                        cmp.select_next_item()
-                      -- elseif check_backspace() then
-                      --   fallback()
-                      else
-                        fallback()
-                      end
-                    end,
-                    ['<S-Tab>'] = function(fallback)
-                      if cmp.visible() then
-                        cmp.select_prev_item()
-                      else
-                        fallback()
-                      end
-                    end,
-                },
-            })
-        end
-    }
+MiniDeps.add({
+    source = 'nvim-treesitter/nvim-treesitter',
+    hooks = { post_checkout = function() vim.cmd('TSUpdate') end },
+})
+require('nvim-treesitter.configs').setup({
+    ensure_installed = 'all',
+    highlight = { enable = true },
 })
 
+MiniDeps.add({
+    source = 'nvim-telescope/telescope.nvim',
+    depends = {'nvim-lua/plenary.nvim'}
+})
+require 'telescope'.setup({
+    defaults = {
+        preview = false,
+        mappings = {
+            i = {
+            ['<esc>'] = require('telescope.actions').close, -- close on single <esc> press (instead of two)
+            },
+        },
+    },
+    pickers = {
+        git_files = {
+            theme = 'ivy',
+        },
+    buffers = {
+        theme = 'ivy',
+        sort_mru = true,
+    },
+    live_grep = {
+          theme = 'ivy',
+        preview = true
+    }
+  },
+})
+vim.keymap.set('n', '<Leader>e', require('telescope.builtin').git_files)
+vim.keymap.set('n', '<Leader>b', require('telescope.builtin').buffers)
+vim.keymap.set('n', '<Leader>l', require('telescope.builtin').live_grep) -- requires ripgrep
 -- Reopen last Telescope window, super useful for live grep
 vim.keymap.set("n", ";", "<cmd>lua require('telescope.builtin').resume(require('telescope.themes').get_ivy({}))<cr>", opts)
+
+MiniDeps.add({
+    source = 'neovim/nvim-lspconfig',
+    -- TODO: migrate to LSP Zero one fine day
+    depends = {'hrsh7th/nvim-cmp',
+               'hrsh7th/cmp-buffer',
+               'hrsh7th/cmp-nvim-lsp',
+               'hrsh7th/cmp-nvim-lsp-signature-help',
+               'williamboman/mason-lspconfig.nvim',
+               'williamboman/mason.nvim'
+}})
+
+-- Install LSP servers from within nvim, check the status with :LspInfo and :LspInstallInfo
+require('mason').setup()
+require('mason-lspconfig').setup({
+    ensure_installed = { 'clangd' },
+    automatic_installation = true
+})
+
+-- Keymaps to expose some LSP features, many other functions are available ...
+local function on_attach(_, bufnr)
+    local opts = { buffer = bufnr }
+    -- vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts) -- neovim 0.10 sets this by default
+    vim.keymap.set('n', 's', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 't', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', 'S', ':ClangdSwitchSourceHeader<CR>', opts)
+    vim.keymap.set('n', 'R', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<Leader>r', vim.lsp.buf.rename, opts)
+    vim.keymap.set('n', '<C-p>', vim.diagnostic.goto_prev, opts)
+    vim.keymap.set('n', '<C-n>', vim.diagnostic.goto_next, opts)
+end
+
+-- Advertise capabilities to LSP server
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+-- Disable messages in sign column by LSP plugin, virtual text does the job nicely
+vim.diagnostic.config({signs = false})
+
+require('lspconfig')['clangd'].setup({
+    on_attach = on_attach,
+    capabilities = capabilities
+})
+
+-- Note from https://clangd.llvm.org/installation.html:
+-- "clangd will look in the parent directories of the files you edit looking
+-- for it, and also in subdirectories named build/. For example, if editing
+-- $SRC/gui/window.cpp, we search in $SRC/gui/, $SRC/gui/build/, $SRC/,
+-- $SRC/build/,"
+-- If this becomes too annoying, we could pass --compile-commands-dir=<string>
+-- to clangd above (--> "cmd")
+
+local check_backspace = function()
+    local col = vim.fn.col '.' - 1
+    return col == 0 or vim.fn.getline('.'):sub(col, col):match '%s'
+end
+
+local cmp = require 'cmp'
+cmp.setup({
+    -- The order controls the preference for specific sources
+    sources = {
+        {name = 'nvim_lsp'},
+        {name = 'nvim_lsp_signature_help'},
+        {name = 'buffer'}
+    },
+    mapping = {
+        -- Overload tab for a natural completion experience
+        ['<Tab>'] = function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          -- elseif check_backspace() then
+          --   fallback()
+          else
+            fallback()
+          end
+        end,
+        ['<S-Tab>'] = function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          else
+            fallback()
+          end
+        end,
+    },
+})
