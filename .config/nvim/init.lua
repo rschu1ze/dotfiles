9af3db7fc54780cc9c277550a2ce938af3f0e7a5
Robert Schulze <robert@clickhouse.com>
2025-01-23 17:47:09 +0000

    Sorting

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -134,7 +134,7 @@ require('mini.pick').setup({
     toggle_preview = '',
   },
   options = {
-    content_from_bottom = true,
+    -- content_from_bottom = true,
     use_cache = true,
   },
   window = {
