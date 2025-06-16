24c01dfcf33e6ad09c3a7b710e22e7f8f845801c
Robert Schulze <robert@clickhouse.com>
2025-06-16 06:27:43 +0000

    Re-introduce mini.pick

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -100,6 +100,44 @@ require("lazy").setup({
                 require("mini.pairs").setup()
                 require("mini.surround").setup()
                 require("mini.trailspace").setup()
+                -- Going back to what worked, see comments about snacks picker and fzf lua below
+                require('mini.pick').setup({
+                    mappings = {
+                    -- intentionally disable all other bindings explicitly ...
+                        caret_left  = '',
+                        caret_right = '',
+                        choose            = '<CR>',
+                        choose_in_split   = '',
+                        choose_in_tabpage = '',
+                        choose_in_vsplit  = '',
+                        choose_marked     = '',
+                        delete_char       = '<BS>',
+                        delete_char_right = '',
+                        delete_left       = '',
+                        delete_word       = '',
+                        mark     = '',
+                        mark_all = '',
+                        move_down  = '<Tab>',
+                        move_up    = '<S-Tab>',
+                        refine        = '';
+                        refine_marked = '',
+                        scroll_down  = '',
+                        scroll_left  = '',
+                        scroll_right = '',
+                        scroll_up    = '',
+                        stop = '<Esc>',
+                        toggle_info    = '',
+                        toggle_preview = '',
+                    },
+                    window = {
+                        config = {
+                            width = 1000, -- max
+                            height = 10,
+                        },
+                    }
+                })
+                vim.keymap.set('n', '<Leader>e', function() require('mini.pick').builtin.files() end)
+                vim.keymap.set('n', '<Leader>b', function() require('mini.pick').builtin.buffers() end)
             end
         },
         -- UTTERLY BROKEN .. I tab-select some file, press <Enter>, and some other file is opened. UNUSABLE.
@@ -136,13 +174,14 @@ require("lazy").setup({
         --         }
         --     }
         -- },
-        {
-            "folke/snacks.nvim",
-            keys = {
-                { "<leader>b", function() Snacks.picker.buffers() end, desc = "Buffers" },
-                { "<leader>e", function() Snacks.picker.git_files() end, desc = "Explorer" }
-            }
-        },
+        -- WTF IS WRONG HERE. WHY DOES THIS PICKER HAVE THE SAME PROBLEM?
+        -- {
+        --     "folke/snacks.nvim",
+        --     keys = {
+        --         { "<leader>b", function() Snacks.picker.buffers() end, desc = "Buffers" },
+        --         { "<leader>e", function() Snacks.picker.git_files() end, desc = "Explorer" }
+        --     }
+        -- },
         {"ethanholz/nvim-lastplace", opts = {}}, -- https://github.com/neovim/neovim/issues/16339
         {"FabijanZulj/blame.nvim", opts = {}},
         {"hiphish/rainbow-delimiters.nvim"},
---
+++ .config/nvim/lua/plugins/gruvbox.lua
@@ -0,0 +1,7 @@
+return
+{
+	"ellisonleao/gruvbox.nvim",
+	config = function()
+		vim.cmd('colorscheme gruvbox')
+	end
+}
